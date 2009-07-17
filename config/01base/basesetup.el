b75a5dacef75bf90d90dad02f7b422bb48c2eb1a
crazycode <crazycode@gmail.com>
2009-07-17 09:51:16 +0800

    turn menu-bar off default, you can use C-F10 to toggle the menu-bar.

--- config/01base/basesetup.el
+++ config/01base/basesetup.el
@@ -174,6 +174,7 @@ scroll-conservatively 10000)
 ;; ;; make #! scripts executable after saving them
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 
+(menu-bar-mode 0)  ;; 默认不打开菜单条，通过C-F10来切换打开
 
 ;; 有关界面和字体的配置
 (add-hook 'after-make-frame-functions
@@ -185,10 +186,6 @@ scroll-conservatively 10000)
                 (progn
                   (split-window-horizontally)
                   (enlarge-window-horizontally 15)
-                  (menu-bar-mode 1)  ;; 图形模式下打开菜单条
-                  )
-                (progn
-                  (menu-bar-mode 1)  ;; 终端模式下open菜单条
                   )
               )
             ))
