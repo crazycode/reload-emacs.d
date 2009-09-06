548a38f380723afb26dbecc7d28495b071994336
crazycode <crazycode@gmail.com>
2009-09-06 15:28:47 +0800

    update yasnippet. modify jde.el

--- config/25jdee/jde.el
+++ config/25jdee/jde.el
@@ -1,5 +1,6 @@
 (setq defer-loading-jde t)
 (setq jde-plugins-directory "~/.emacs.d/vendor/25jdee/jde-plugins")
+(setq jde-compile-enable-kill-buffer t) ;;编译成功时关闭提示buffer
 
 ;; 编辑.java文件时加载jde
 (if defer-loading-jde
