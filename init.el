07e2041a00a29f4c20020de64dff8f0f8665170e
crazycode <crazycode@gmail.com>
2009-11-09 22:58:08 +0800

    remote jde.

--- init.el
+++ init.el
@@ -22,7 +22,6 @@
 
 (load "~/.emacs.d/my-custom.el")
 (mapc 'load (directory-files "~/.emacs.d/config/01base" t "\.el$"))
-(mapc 'load (directory-files "~/.emacs.d/config/25jdee" t "\.el$"))
 (mapc 'load (directory-files "~/.emacs.d/config/50webdev" t "\.el$"))
 (mapc 'load (directory-files "~/.emacs.d/config/70emacsonrails" t "\.el$"))
 (mapc 'load (directory-files "~/.emacs.d/config/85authoring" t "\.el$"))
