a344a4c01ceb9d29a85a333401f69096d38177ef
crazycode <crazycode@gmail.com>
2010-01-17 02:15:44 +0800

    add org-mode config.

---
+++ config/85authoring/markdown-mode.el
@@ -0,0 +1,4 @@
+(autoload 'markdown-mode "markdown-mode.el"
+      "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+          (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
