5cd4342e2a57c19d099111cfc9b250a692b58369
crazycode <crazycode@gmail.com>
2010-01-29 00:08:51 +0800

    dd htmlize.

--- config/85authoring/muse.el
+++ config/85authoring/muse.el
@@ -1,3 +1,4 @@
+(require 'htmlize)
 (require 'muse-mode)
 
 (require 'muse-html)         ;添加html格式的支持
@@ -15,3 +16,11 @@
       '(("my-wiki"
          ("~/gitworks/my-wiki" :default "index")
          (:base "html" :path "~/gitworks/my-wiki/publish"))))
+
+;; 默认情况下 TAB 键会在各个链接之间跳转，我觉得没有什么用处，而且很烦人，我更喜欢让 TAB 键帮我缩进到正确的位置
+(define-key muse-mode-map [tab] 'indent-for-tab-command)
+
+;; TAB 键行使 outline 操作
+;;(require 'org)
+;;(define-key muse-mode-map (kdb "<tab>") 'org-cycle)
+;;(define-key muse-mode-map (kbd "<S-iso-lefttab>") 'org-shifttab)
