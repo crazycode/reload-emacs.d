3c78c9c56d84f9a156f5872eb877068a4b848879
crazycode <crazycode@gmail.com>
2010-04-13 17:56:33 +0800

    update org-mode.el

--- config/01base/org-mode.el
+++ config/01base/org-mode.el
@@ -1,3 +1,18 @@
+
+(add-to-list 'auto-mode-alist '("\.\(org\|org_archive\|txt\)$" . org-mode))
+(define-key global-map "C-cl" 'org-store-link)
+(define-key global-map "C-ca" 'org-agenda)
+(setq org-log-done t)
 (setq org-hide-leading-stars t)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done 'time)
+
+(setq org-todo-keywords
+      (list "TODO(t)" "STARTED(s!)" "WAITING(w@)" "|" "CANCELED(c)" "DONE(d)"))
+(setq org-agenda-files
+      (list "~/Dropbox/personal.org"
+            "~/Dropbox/lab.org"
+            "~/Dropbox/fost.org"
+            "~/Dropbox/todo.org"))
+(defun todo ()
+  (interactive)
+  (find-file "~/git/org/todo.org")
+  )
