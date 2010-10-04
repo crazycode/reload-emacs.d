5d3562396be732978b8e5baba2c991ba0d59b909
crazycode <crazycode@gmail.com>
2010-10-04 20:42:46 +0800

    fix daemon's key error.

--- config/01base/term.el
+++
@@ -1,8 +0,0 @@
-(add-hook 'after-make-frame-functions
-          (lambda (new-frame)
-            (select-frame new-frame)
-            (when (and (not (window-system frame))
-                       (string-match "^xterm" (getenv "TERM")))
-              (require 'xterm-title)
-              (xterm-title-mode 1))
-            ))
