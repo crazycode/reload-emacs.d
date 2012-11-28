166570dde116b878c155c86adda4bfb7d2ffa578
crazycode <crazycode@gmail.com>
2012-11-28 14:44:51 +0800

    update yari.

--- config/70emacsonrails/ruby-mode.el
+++ config/70emacsonrails/ruby-mode.el
@@ -13,22 +13,22 @@
 ;;   ;; always indent line
 ;;   (indent-for-tab-command))
 
-;;  (setq hippie-expand-try-functions-list
-;;        '(
-;;          yas-hippie-try-expand
-;;          try-expand-dabbrev
-;;          try-expand-dabbrev-visible
-;;          try-expand-dabbrev-all-buffers
-;;          try-expand-dabbrev-from-kill
-;;          try-complete-file-name
-;;          try-complete-file-name-partially
-;;          try-complete-lisp-symbol
-;;          try-complete-lisp-symbol-partially
-;;          try-expand-line
-;;          try-expand-line-all-buffers
-;;          try-expand-list
-;;          try-expand-list-all-buffers
-;;          try-expand-whole-kill))
+(setq hippie-expand-try-functions-list
+      '(
+        yas-hippie-try-expand
+        try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name
+        try-complete-file-name-partially
+        try-complete-lisp-symbol
+        try-complete-lisp-symbol-partially
+        try-expand-line
+        try-expand-line-all-buffers
+        try-expand-list
+        try-expand-list-all-buffers
+        try-expand-whole-kill))
 
 ;;(global-set-key (kbd "<tab>") 'crazycode/indent-and-complete)
 
@@ -37,7 +37,7 @@
 
 (require 'yari)
 (defun ri-bind-key ()
-   (local-set-key [f1] 'yari-anything))
+   (local-set-key [f1] 'yari))
 
 (add-hook 'ruby-mode-hook 'ri-bind-key)
 
